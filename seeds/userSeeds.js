b9999eb84adf52039900d0a095835fd9632ae9ee
SoftPoachedEggs <115499632+SoftPoachedEggs@users.noreply.github.com>
2023-04-20 00:46:20 -0400

    fixed image

--- seeds/userSeeds.js
+++ seeds/userSeeds.js
@@ -2,22 +2,18 @@ const { User } = require("../models");
 
 const seedData = [
   {
-    user_id: `1`,
     username: `TechyMike`,
     password: `12345`,
   },
   {
-    user_id: `2`,
     username: "TechnoSteve",
     password: `Steve`,
   },
   {
-    user_id: `3`,
     username: `RobotRandall`,
     password: `Randall`,
   },
   {
-    user_id: `4`,
     username: `Binary_Britt`,
     password: `Britt`,
   },
